d50c1910869604e46e616ea2731da52c8992c97b
Rhishabh Hattarki <rhattark@asu.edu>
2023-10-18 17:23:22 -0700

    visualization correlation matrix

--- wf_visualization.py
+++ wf_visualization.py
@@ -67,3 +67,32 @@ print("-------------------------------------------------------------------------
 summary = 'Quantitative summary\n\n' + quant_summary.to_string() + '\n\nQualitative summary\n\n' + qual_summary.to_string()
 with open('data_processed/summary.txt', 'w') as file:
     file.write(summary)
+
+# get correlation between quantitative features
+import numpy as np
+correlation_matrix = quantitative.corr()
+cols = [col for col in correlation_matrix.columns]
+trunc_corr_matrix = [['Column'] + cols]
+
+for col in cols:
+    trunc_corr_matrix.append([col])
+
+for i in range(len(cols)):
+    for j in range(i+1):
+        trunc_corr_matrix[i+1].append(correlation_matrix.iloc[i, j])
+
+pretty_corr_matrix = ''
+
+for row in trunc_corr_matrix:
+    row_str = ''
+    for col in row:
+        row_str += f'{col:<25}'
+    pretty_corr_matrix += row_str + '\n'
+
+print("------------------------------------- Correlation matrix ----------------------------------------------")
+print(pretty_corr_matrix)
+print("-------------------------------------------------------------------------------------------------------")
+
+# write correlation matrix to txt file
+with open('data_processed/correlations.txt', 'w') as file:
+    file.write(pretty_corr_matrix)
