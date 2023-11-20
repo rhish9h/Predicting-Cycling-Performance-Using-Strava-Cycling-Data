68da92e3f444a9b258328a7ede5f2985977a5912
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-20 08:55:31 -0700

    evaluation basic

--- wf_ml_evaluation.py
+++ wf_ml_evaluation.py
@@ -6,21 +6,36 @@ import wf_ml_training
 import wf_ml_prediction
 from sklearn.metrics import mean_squared_error, r2_score
 
-def evaluate():
-    zones_ftp_power_agg = pd.read_csv('data_processed/zones_ftp_power_agg.csv')
+def get_errors(y_test, y_pred):
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    return mse, r2
+
+def split_and_transform(filepath):
+    zones_ftp_power_agg = pd.read_csv(f'data_processed/{filepath}.csv')
     X_train, X_test, y_train, y_test = train_test_split(zones_ftp_power_agg.iloc[:, :-1], zones_ftp_power_agg.iloc[:, -1], test_size=0.2, random_state=42)
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    wf_ml_training.train_model(X_train_scaled, y_train, LinearRegression(), 'linear_regression_power_small')
-    y_pred = wf_ml_prediction.predict('linear_regression_power_small', X_test_scaled)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+    return X_train_scaled, X_test_scaled, y_train, y_test
 
-    # Print evaluation metrics
-    print(f'Mean Squared Error: {mse}')
-    print(f'R-squared: {r2}')
+def train_predict_get_errors(filepath, ml_model):
+    X_train_scaled, X_test_scaled, y_train, y_test = split_and_transform(filepath)
+    wf_ml_training.train_model(X_train_scaled, y_train, ml_model, filepath)
 
+    y_pred = wf_ml_prediction.predict(filepath, X_test_scaled)
+    mse, r2 = get_errors(y_test, y_pred)
+    return mse, r2
+
+def evaluate():
+    summary_columns = ['Dataset', 'Method', 'MSE', 'R2']
+    summary = pd.DataFrame(columns=summary_columns)
+    mse, r2 = train_predict_get_errors('zones_ftp_power_agg', LinearRegression())
+    summary.loc[len(summary)] = ['zones_ftp_power_agg', 'Linear Regression', mse, r2]
+    print('-----------------------------Summary-----------------------------')
+    print(summary)
+    
 
 if __name__ == '__main__':
     evaluate()
