360e3c77769851b63d01db3f91d7a13d04986085
Rhishabh Hattarki <rhattark@asu.edu>
2023-11-20 09:10:28 -0700

    ridge and lasso added

--- wf_ml_evaluation.py
+++ wf_ml_evaluation.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 import wf_ml_training
 import wf_ml_prediction
 from sklearn.metrics import mean_squared_error, r2_score
@@ -20,11 +22,11 @@ def split_and_transform(filepath):
     X_test_scaled = scaler.transform(X_test)
     return X_train_scaled, X_test_scaled, y_train, y_test
 
-def train_predict_get_errors(filepath, ml_model):
+def train_predict_get_errors(filepath, ml_model, model_name):
     X_train_scaled, X_test_scaled, y_train, y_test = split_and_transform(filepath)
-    wf_ml_training.train_model(X_train_scaled, y_train, ml_model, filepath)
+    wf_ml_training.train_model(X_train_scaled, y_train, ml_model, filepath + '_' + model_name)
 
-    y_pred = wf_ml_prediction.predict(filepath, X_test_scaled)
+    y_pred = wf_ml_prediction.predict(filepath + '_' + model_name, X_test_scaled)
     mse, r2 = get_errors(y_test, y_pred)
     return mse, r2
 
@@ -32,10 +34,12 @@ def evaluate():
     summary_columns = ['Dataset', 'Method', 'MSE', 'R2']
     summary = pd.DataFrame(columns=summary_columns)
     datasets = ['zones_ftp_power_agg', 'zones_ftp_hr_agg', 'zones_ftp_power_hr_agg']
+    models = [('Linear Regression', LinearRegression()), ('Ridge', Ridge()), ('Lasso', Lasso())]
 
     for dataset in datasets:
-        mse, r2 = train_predict_get_errors(dataset, LinearRegression())
-        summary.loc[len(summary)] = [dataset, 'Linear Regression', mse, r2]
+        for model_name, model in models:
+            mse, r2 = train_predict_get_errors(dataset, model, model_name)
+            summary.loc[len(summary)] = [dataset, model_name, mse, r2]
 
     print('-----------------------------Summary-----------------------------')
     print(summary)
